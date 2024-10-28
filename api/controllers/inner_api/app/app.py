5d04653a77f7ed8d8e905e6e1acc4033d1ebb7a5
pudgeli <pudgeli@tencent.com>
2024-10-28 17:43:00 +0800

    创建app时添加code返回

--- api/controllers/inner_api/app/app.py
+++ api/controllers/inner_api/app/app.py
@@ -42,7 +42,7 @@ class App(Resource):
             args["icon"] = app_template.get("icon", "🤖")
             args["icon_background"] = app_template.get("icon_background", "#FFEAD5")
         else:
-            return {"message": f"不支持的应用模式: {args['mode']}"}, 200
+            return {"code":-1,"message": f"不支持的应用模式: {args['mode']}"}, 200
 
         # 根据email查找account
         account = None
@@ -50,12 +50,12 @@ class App(Resource):
             account = Account.query.filter_by(email=args["email"]).first()
         
         if not account:
-            return {"message": "未找到对应的租户账户。"}, 200
+            return {"code":-1,"message": "未找到对应的租户账户。"}, 200
             
         account.current_tenant_id = args["tenant_id"]
         
         if not args["dataset_name"] or args["dataset_name"] == "":
-            return {"message": "知识库名字为空"}, 200
+            return {"code":-1,"message": "知识库名字为空"}, 200
         try:
             dataset = DatasetService.create_empty_dataset(
                 tenant_id=args["tenant_id"],
@@ -66,7 +66,7 @@ class App(Resource):
             )
             args["dataset_id"] = dataset.id
         except DatasetNameDuplicateError:
-            return {"message": "The dataset name already exists. Please modify your dataset name"}, 200
+            return {"code":-1,"message": "The dataset name already exists. Please modify your dataset name"}, 200
 
         app_service = AppService() 
         api_key_service = AppKeyService()
@@ -74,9 +74,9 @@ class App(Resource):
             app = app_service.create_app(args["tenant_id"], args, account)
             # 创建token
             api_token = api_key_service.newApiKey(account,str(app.id),"app","app_id","app-")
-            return {"message": "应用创建成功。", "app_id": str(app.id), "api_key":api_token.token,"dataset_id": args["dataset_id"]}, 200
+            return {"code":0,"message": "应用创建成功。", "app_id": str(app.id), "api_key":api_token.token,"dataset_id": args["dataset_id"]}, 200
         except Exception as e:
-            return {"message": f"创建应用失败: {str(e)}"}, 200
+            return {"code":-1,"message": f"创建应用失败: {str(e)}"}, 200
     
     @setup_required
     @inner_api_only
